ce3aafca25838e271a4274cdc0f3f3ee35a9bbd0
arielnisim <141743438+arielnisim@users.noreply.github.com>
2025-07-08 13:15:55 +0300

    666

--- src/lib/clients/supabaseBrowserClient.ts
+++ src/lib/clients/supabaseBrowserClient.ts
@@ -1,9 +1,18 @@
+"use client";
+
 import { createBrowserClient } from "@supabase/ssr";
 
 const supabaseBrowserClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: true,
+      },
+    }
   );
 
 export default supabaseBrowserClient;
