e668f2c0648a388ca7d0b530bc01277a477924d4
Ariel Gabay <o0o0.wvvw.0o0o@gmail.com>
2025-07-08 17:15:41 +0300

    ooo

--- src/lib/hooks/useSWRUpdateAdmin.tsx
+++ src/lib/hooks/useSWRUpdateAdmin.tsx
@@ -1,14 +1,14 @@
 // src/lib/hooks/useSWRUpdateUser.ts
 import useSWRMutation from "swr/mutation";
 
-type UpdateUserArgs =
+export type UpdateUserArgs =
   | { password: string; metadata?: never }
   | { metadata: Record<string, any>; password?: never };
 
 export function useSWRUpdateAdmin() {
   return useSWRMutation(
     "/api/auth/update",
-    async (_, { arg }: { arg: UpdateUserArgs }) => {
+    async (_key, { arg }: { arg: UpdateUserArgs }) => {
       const res = await fetch("/api/auth/updateAdmin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
