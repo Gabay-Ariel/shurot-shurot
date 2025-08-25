feefb2ec3f1f33de060e9ecff73f3d7dc8f3c267
arielnisim <141743438+arielnisim@users.noreply.github.com>
2025-08-25 13:09:52 +0300

    //

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -11,7 +11,7 @@ export async function GET(req: Request) {
   const res = await fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=${encodeURIComponent(
       q
-    )}&key=${process.env.YOUTUBE_API_KEY}`
+    )}&key=AIzaSyAfy_RtNVhbZz5CE0xnMDfUY5LtsNwR9mw`
   );
 
   const data = await res.json();
