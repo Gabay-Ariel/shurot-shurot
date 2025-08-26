4c89e904fd51333c3133a460f93dcc6485c38ad3
arielnisim <141743438+arielnisim@users.noreply.github.com>
2025-08-26 13:16:11 +0300

    m

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -59,7 +59,8 @@ export default function YouTubeSearchPage() {
         <iframe
           width="100%"
           height={400}
-          src={`https://www.youtube.com/embed/${videoId}`}
+          src={"/aaaa.mp4"}
+          //   src={`https://www.youtube.com/embed/${videoId}`}
           title={query}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
